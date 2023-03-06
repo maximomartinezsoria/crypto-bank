92141d25c82e7b7218e9d5b146f9c43dd72d76bd
Maximo Martinez Soria <mmartinez@40q.agency>
2023-03-06 10:40:51 +0100

    Add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,39 @@
-async function main() {}
+import { Contract } from 'ethers';
+import * as fs from 'fs';
+import { ethers } from 'hardhat';
+
+async function main() {
+	const Bank = await ethers.getContractFactory('Bank');
+	const bank = await Bank.deploy();
+
+	await bank.deployed();
+
+	writeDeploymentInfo(bank);
+}
+
+async function writeDeploymentInfo(contract: Contract) {
+	const signerAddress = await contract.signer.getAddress();
+	const data = {
+		contract: {
+			address: contract.address,
+			signerAddress,
+			abi: contract.interface.format(),
+		},
+	};
+
+	const content = JSON.stringify(data, null, 2);
+
+	fs.writeFile(
+		'deployment.json',
+		content,
+		{
+			encoding: 'utf-8',
+		},
+		console.log
+	);
+}
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
